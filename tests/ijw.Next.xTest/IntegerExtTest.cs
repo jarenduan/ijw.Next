85b5deffe6da2d57627dcaaaaff1e322b876d554
jaren@5510 <jarenduan@gmail.com>
2018-08-29 13:47:35 +0800

    整理代码格式

--- tests/ijw.Next.xTest/IntegerExtTest.cs
+++ tests/ijw.Next.xTest/IntegerExtTest.cs
@@ -4,11 +4,10 @@ using System.Text;
 using Xunit;
 
 namespace ijw.Next.xTest {
-    public class IntegerExtTest
-    {
+    public class IntegerExtTest {
         [Fact]
         public void ToTotalTest() {
-            int[] numbers = new int[] {0, 1, 2, 3, 4, 5 };
+            int[] numbers = new int[] { 0, 1, 2, 3, 4, 5 };
             Assert.Equal(numbers, 0.ToTotal(6));
 
             numbers = new int[] { 2, 3, 4, 5 };
