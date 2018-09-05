25fdafb6d846ec6d7b9f9dea0bd053c222f848dc
Jaren@HomePC <jarenduan@outlook.com>
2018-09-05 22:14:08 +0800

    增加string.asFileInfo功能

--- src/ijw.Next.IO/StringExt.cs
+++ src/ijw.Next.IO/StringExt.cs
@@ -1,10 +1,24 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 
 namespace ijw.Next.IO {
     /// <summary>
     /// 
     /// </summary>
     public static class StringExt {
+        /// <summary>
+        /// 获取字符串代表的FileInfo. 
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns>指代的FileInfo</returns>
+        /// <exception cref="FileNotFoundException">字符串不是有效的文件路径时, 抛出此异常. </exception>
+        /// <remarks>即使返回有效的FileInfo, 也并不保证之后文件仍继续存在, 应考虑使用try-catch进行后续操作. </remarks>
+        public static FileInfo AsFileInfo(this string filepath) {
+            filepath.MustExistSuchFile();
+            FileInfo fi = new FileInfo(filepath);
+            return fi;
+        }
+
         /// <summary>
         /// 使用指定编码将字符串写入指定文本文件
         /// </summary>
