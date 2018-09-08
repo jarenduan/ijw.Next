9d172b1b5d23d0d5ca60c7141f005a5a4b278cd3
Jaren@HomePC <jarenduan@outlook.com>
2018-09-08 21:55:15 +0800

    new version

--- src/ijw.Next.Serialization.Binary/FileInfoExt.cs
+++ src/ijw.Next.Serialization.Binary/FileInfoExt.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 
 namespace ijw.Next.Serialization.Binary {
     /// <summary>
