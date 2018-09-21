d83bf73e4c9df38b601ce05c97839234daba0f8e
Jaren@HomePC <jarenduan@outlook.com>
2018-09-21 08:49:59 +0800

    增加string的类型判断函数

--- src/ijw.Next/BasicTypeExt/StringExt.cs
+++ src/ijw.Next/BasicTypeExt/StringExt.cs
@@ -353,6 +353,45 @@ namespace ijw.Next {
         #endregion
 
         #region  Is other type
+        /// <summary>
+        /// 是否能解析成32位整数
+        /// </summary>
+        /// <param name="aString"></param>
+        /// <returns>是返回true, 反之返回false</returns>
+        public static bool IsInteger32(this string aString) {
+            return int.TryParse(aString, out int i);
+        }
+
+        /// <summary>
+        /// 是否能解析成单精度浮点数
+        /// </summary>
+        /// <param name="aString"></param>
+        /// <returns></returns>
+        public static bool IsSingle(this string aString) {
+            return float.TryParse(aString, out var f);
+        }
+
+        /// <summary>
+        /// 是否能解析成双精度浮点数
+        /// </summary>
+        /// <param name="aString"></param>
+        /// <returns></returns>
+        public static bool IsDouble(this string aString) {
+            return double.TryParse(aString, out var d);
+        }
+
+        /// <summary>
+        /// 是否能解析成日期时间类型
+        /// </summary>
+        /// <param name="aString"></param>
+        /// <returns></returns>
+        public static bool IsDateTime(this string aString) {
+            return DateTime.TryParse(aString, out var d);
+        }
+
+        #endregion
+
+        #region Is Null Or Empty Or space
 
         /// <summary>
         /// 判断是否是Null或者空字符串
@@ -380,15 +419,7 @@ namespace ijw.Next {
             return true;
         }
 
-        /// <summary>
-        /// 是否是32位整数
-        /// </summary>
-        /// <param name="aString"></param>
-        /// <returns>是返回true, 反之返回false</returns>
-        public static bool IsInteger32(this string aString) {
-            return int.TryParse(aString, out int i);
-        }
-#endregion
+        #endregion New Region
 
         /// <summary>
         /// 统计含有多少指定的子字符串
