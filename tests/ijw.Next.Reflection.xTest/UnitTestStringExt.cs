7d723da4df474cc46e5211a7465a1b8d37398fd6
Jaren Duan <jarenduan@gmail.com>
2019-02-07 13:05:15 +0800

    add string.to test

---
+++ tests/ijw.Next.Reflection.xTest/UnitTestStringExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ijw.Next.Reflection.xTest {
+    public class UnitTestStringExt {
+        [Fact]
+        public void TestTo() {
+            int i = 100;
+            var x1 = "100".To<int>();
+            Assert.Equal(i, x1);
+
+            var y1 = "09".ToNullable<int>();
+            Assert.Equal(9, y1.Value);
+
+            var y2 = "".ToNullable<int>();
+            Assert.Null(y2);
+        }
+    }
+}
