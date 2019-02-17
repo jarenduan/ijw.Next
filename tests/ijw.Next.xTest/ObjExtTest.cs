1d06c18bcf9df131d49d64ac6d170a73b270bf6b
Jaren Duan <jarenduan@gmail.com>
2019-02-17 12:31:27 +0800

    add obj.setPropertyValue test

---
+++ tests/ijw.Next.xTest/ObjExtTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ijw.Next.xTest {
+    public class ObjExtTest {
+        [Fact]
+        public void GetPropertyTest() {
+            TestClassA a = new TestClassA();
+
+            a.SetPropertyValue("Name", "B");
+
+            Assert.Equal("B", a.Name);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                a.SetPropertyValue("name", "b")
+            );
+        }
+
+        private class TestClassA {
+            public string Name { get; set; } = "A";
+            public int Age { get; set; } = 12;
+            public void SayHello() {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
