f439e60b16c632379ce4323c3a9f35ce725c7422
Jaren Duan <jarenduan@gmail.com>
2019-02-19 12:55:09 +0800

    reorder parameters of ToAllEnumStrings method. use c# 8.0 to rewrite codes

--- src/ijw.Next/Helpers/IjwHelper.cs
+++ src/ijw.Next/Helpers/IjwHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace ijw.Next {
     /// <summary>
     /// 一些不好分类的帮助方法
@@ -17,29 +18,46 @@ namespace ijw.Next {
         /// <param name="endAtPython">结束索引. 该处字符将不包括在返回结果中. 0代表第一个字符, 负数代表倒数第几个字符(-1表示倒数第一个字符), null代表结尾. 默认值为null.</param>
         public static void PythonStartEndCalculator(int length, out int start, out int end, int? startAtPython = 0, int? endAtPython = null) {
             //endAt.ShouldNotEquals(0);
-            if (startAtPython == null) {
-                startAtPython = 0;
+
+            if (startAtPython is null) {
+                start = 0;
             }
             else if (startAtPython < 0) {
-                startAtPython = length + startAtPython;
-            }
-
-            if (endAtPython == null) {
-                endAtPython = length - 1;
-            }
-            else if (endAtPython < 0) {
-                endAtPython = length + endAtPython - 1;
+                start = length + startAtPython.Value;
             }
             else {
-                endAtPython--;
+                start = startAtPython.Value;
             }
 
-            //if (startAt > endAt) {
-            //    throw new Exception("start index > end index.");
-            //}
+            if (endAtPython is null) {
+                end = length - 1;
+            }
+            else if (endAtPython < 0) {
+                end = length + endAtPython.Value - 1;
+            }
+            else {
+                end = endAtPython.Value - 1;
+            }
+        }
 
-            start = startAtPython.Value;
-            end = endAtPython.Value;
+        /// <summary>
+        /// 输出一个包含集合中所有元素的字符串, 默认形如[a1, a2, a3, [a41, a42, a43], a5]
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">指定的集合</param>
+        /// <param name="separator">元素之间的分隔符, 默认是", "</param>
+        /// <param name="prefix">第一个元素之前的字符串, 默认是"["</param>
+        /// <param name="postfix">最后一个元素之后的字符串, 默认是"]"</param>
+        /// <returns></returns>
+        public static string ToAllEnumStrings<T>(IEnumerable<T> collection,
+                                                 string separator = ", ",
+                                                 string prefix = "[",
+                                                 string postfix = "]") {
+            return ToAllEnumStrings(collection,
+                                    (item) => item is null ? "[NULL]" : item.ToString(),
+                                    separator,
+                                    prefix,
+                                    postfix);
         }
 
         /// <summary>
@@ -52,21 +70,25 @@ namespace ijw.Next {
         /// <param name="postfix">最后一个元素之后的字符串, 默认是"]"</param>
         /// <param name="transform">对于每个元素, 输出字符串之前进行一个操作.默认为null, 代表调用ToString().</param>
         /// <returns></returns>
-        public static string ToAllEnumStrings<T>(IEnumerable<T> collection, string separator = ", ", string prefix = "[", string postfix = "]", Func<T, string> transform = null) {
+        public static string ToAllEnumStrings<T>(IEnumerable<T> collection,
+                                                 Func<T, string> transform,
+                                                 string separator = ", ",
+                                                 string prefix = "[",
+                                                 string postfix = "]") {
             StringBuilder sb = new StringBuilder(prefix);
             foreach (var item in collection) {
-                if (item is IEnumerable<T> ienum) {
-                    sb.Append(ToAllEnumStrings(ienum, separator, prefix, postfix));
-                }
-                else {
-                    string s = transform == null ? item.ToString() : transform(item);
-                    sb.Append(s);
-                }
+                string s = item switch
+                {
+                    IEnumerable<T> ienum => ToAllEnumStrings(ienum, separator, prefix, postfix),
+                    _                    => transform(item)
+                };
+                sb.Append(s);
                 sb.Append(separator);
             }
             sb.RemoveLast(separator.Length);
             sb.Append(postfix);
             return sb.ToString();
         }
+
     }
 }
