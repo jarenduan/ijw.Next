34afe5ad11f712d9364c02c770c0721df3fb36a2
Jaren Duan <jarenduan@gmail.com>
2019-02-27 10:49:56 +0800

    clean codes

--- src/ijw.Next/Contracts/Should/ObjectContractShouldExt.cs
+++ src/ijw.Next/Contracts/Should/ObjectContractShouldExt.cs
@@ -6,15 +6,15 @@ namespace ijw.Next {
     /// </summary>
     public static class ObjectContractShouldExt {
         /// <summary>
-        /// 应该不是Null参数
+        /// 可空类型的实例应该不是Null参数
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="obj"></param>
         /// <param name="paramName">参数的名字</param>
         /// <returns>不是Null,返回true. 反之抛出ArgumentNullException异常</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static bool ShouldBeNotNullArgument<T>(this T obj, string paramName = "") where T : class {
-            if (obj == null) {
+        public static bool ShouldBeNotNullArgument<T>(this T obj, string paramName = "") {
+            if (obj is null) {
                 if (paramName == "") {
                     throw new ArgumentNullException();
                 }
@@ -24,6 +24,7 @@ namespace ijw.Next {
             }
             return true;
         }
+
         /// <summary>
         /// 应该不是Null引用
         /// </summary>
@@ -32,8 +33,8 @@ namespace ijw.Next {
         /// <param name="message">错误信息</param>
         /// <returns>不是Null,返回true. 反之抛出NullReferenceException异常.</returns>
         /// <exception cref="NullReferenceException"></exception>
-        public static bool ShouldBeNotNullReference<T>(this T obj, string message = "") where T : class {
-            if (obj == null) {
+        public static bool ShouldBeNotNullReference<T>(this T obj, string message = "") {
+            if (obj is null) {
                 if (message == "") {
                     throw new NullReferenceException();
                 }
@@ -51,13 +52,9 @@ namespace ijw.Next {
         /// <param name="obj"></param>
         /// <param name="condition">应满足的条件</param>
         /// <returns>满足条件返回true. 反之抛出NotSatisfiedConditionException异常.</returns>
-        /// <exception cref="NotSatisfiedConditionException{T}"></exception>
-        public static bool ShouldSatisfy<T>(this T obj, Predicate<T> condition) {
-            if (!condition(obj)) {
-                throw new NotSatisfiedConditionException<T>(obj, condition);
-            }
-            return true;
-        }
+        /// <exception cref="NotSatisfiedConditionException{T,T}"></exception>
+        public static bool ShouldSatisfy<T>(this T obj, Predicate<T> condition) 
+            => condition(obj) ? true : throw new NotSatisfiedConditionException<T, T>(obj, condition);
 
         /// <summary>
         /// 应该满足指定的条件, 否则抛出指定的异常.
@@ -69,12 +66,20 @@ namespace ijw.Next {
         /// <param name="excpetion">不满足时会抛出的异常</param>
         /// <returns>满足条件返回true, 否则抛出指定的异常.</returns>
         /// <exception cref="Exception"></exception>
-        public static bool ShouldSatisfy<T, TException>(this T obj, Predicate<T> condition, TException excpetion) where TException : Exception {
-            if (!condition(obj)) {
-                throw excpetion;
-            }
-            return true;
-        }
+        public static bool ShouldSatisfy<T, TException>(this T obj, Predicate<T> condition, TException excpetion) where TException : Exception 
+            => condition(obj) ? true : throw excpetion;
+
+#nullable disable
+        /// <summary>
+        /// 应该与指定的对象相等.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="other">用以比较的对象</param>
+        /// <returns>与指定的对象相等返回true, 否则抛出ContractBrokenException异常.</returns>
+        /// <exception cref="ContractBrokenException"></exception>
+        public static bool ShouldEquals<T>(this T obj, T other) 
+            => obj.Equals(other) ? true : throw new ContractBrokenException();
 
         /// <summary>
         /// 应该与指定的对象相等.
@@ -84,26 +89,8 @@ namespace ijw.Next {
         /// <param name="other">用以比较的对象</param>
         /// <returns>与指定的对象相等返回true, 否则抛出ContractBrokenException异常.</returns>
         /// <exception cref="ContractBrokenException"></exception>
-        public static bool ShouldEquals<T>(this T obj, T other) {
-            if (!obj.Equals(other)) {
-                throw new ContractBrokenException();
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// 应该与指定的对象相等.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="obj"></param>
-        /// <param name="other">用以比较的对象</param>
-        /// <returns>与指定的对象相等返回true, 否则抛出ContractBrokenException异常.</returns>
-        /// <exception cref="ContractBrokenException"></exception>
-        public static bool ShouldNotEquals<T>(this T obj, T other) {
-            if (obj.Equals(other)) {
-                throw new ContractBrokenException();
-            }
-            return true;
-        }
+        public static bool ShouldNotEquals<T>(this T obj, T other) 
+            => !obj.Equals(other) ? true : throw new ContractBrokenException();
+#nullable enable
     }
 }
