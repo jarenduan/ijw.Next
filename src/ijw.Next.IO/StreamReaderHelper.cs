176a83f661e2a28dec52b188307d2a6ffa11daf1
Jaren@HomePC <jarenduan@outlook.com>
2019-07-02 13:13:56 +0800

    增加几个重载

--- src/ijw.Next.IO/StreamReaderHelper.cs
+++ src/ijw.Next.IO/StreamReaderHelper.cs
@@ -7,13 +7,29 @@ namespace ijw.Next.IO {
     /// </summary>
     public class StreamReaderHelper {
         /// <summary>
-        /// 使用Unicode字符集打开指定文件.请使用using.
+        /// 使用 UTF8 编码打开指定文件.请使用using.
+        /// </summary>
+        /// <param name="fileinfo">文件信息</param>
+        /// <returns>流读取器</returns>
+        public static StreamReader NewStreamReaderFromFileInfo(FileInfo fileinfo) 
+            => NewStreamReaderFromFileInfo(fileinfo, Encoding.UTF8);
+
+        /// <summary>
+        /// 使用 UTF8 编码打开指定文件.请使用using.
+        /// </summary>
+        /// <param name="fileinfo">文件信息</param>
+        /// <param name="encoding">编码</param>
+        /// <returns>流读取器</returns>
+        public static StreamReader NewStreamReaderFromFileInfo(FileInfo fileinfo, Encoding encoding) 
+            => NewStreamReaderFromFile(fileinfo.FullName, encoding);
+
+        /// <summary>
+        /// 使用 UTF8 编码打开指定文件.请使用using.
         /// </summary>
         /// <param name="filepath">文件路径</param>
         /// <returns>流读取器</returns>
-        public static StreamReader NewStreamReaderFrom(string filepath) {
-            return NewStreamReaderFrom(filepath, Encoding.Unicode);
-        }
+        public static StreamReader NewStreamReaderFromFile(string filepath) 
+            => NewStreamReaderFromFile(filepath, Encoding.UTF8);
 
         /// <summary>
         /// 使用指定的编码方式打开指定文件.请使用using.
@@ -21,7 +37,7 @@ namespace ijw.Next.IO {
         /// <param name="filepath">文件路径</param>
         /// <param name="encoding">文本编码方式</param>
         /// <returns></returns>
-        public static StreamReader NewStreamReaderFrom(string filepath, Encoding encoding) {
+        public static StreamReader NewStreamReaderFromFile(string filepath, Encoding encoding) {
             FileStream fs = new FileStream(filepath, FileMode.Open);
             return new StreamReader(fs, encoding);
         }
