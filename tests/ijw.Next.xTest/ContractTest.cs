90ab1054930237bcaf0a194d014c4258d52b86ca
Jaren@HomePC <jarenduan@outlook.com>
2019-07-03 21:21:13 +0800

    升级nullable 增加nullable 测试

---
+++ tests/ijw.Next.xTest/ContractTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ijw.Next.xTest {
+    public class ContractTest {
+        [Fact]
+        public void ShouldBeNotNullTest() {
+            //Value type never will be null, uncomment line below, you'll get errors.
+            //int i = 10;
+            //i.ShouldBeNotNull();
+
+            int? ii = 10;
+            ii.ShouldBeNotNull();
+
+            string s = "";
+            s.ShouldBeNotNull();
+
+            string? ss = null;
+            Assert.Throws<NullReferenceException>(() => ss.ShouldBeNotNull() );
+        }
+
+        [Fact]
+        public void MustBeNotNullTest() {
+            string s = "";
+            s.MustNotNull();
+        }
+    }
+}
